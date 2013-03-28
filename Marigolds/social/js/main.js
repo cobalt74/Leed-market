a784863dd7666b5a795034b1ea5a0ed9b3df29f1
cobalt74 <cobalt74@gmail.com>
2013-03-29 00:37:53 +0100

    Revert "Social Plugin"
    This reverts commit ea312e56c6644976e9e9e081e9d3538f15a59828.

--- Marigolds/social/js/main.js
+++ Marigolds/social/js/main.js
@@ -2,14 +2,3 @@ function openURL(url){
    window.open(url, '_blank');
    return false;
 }
-
-function social_toggle_div(maindiv, id) {
-  var div = document.getElementById(id);
-  if(div.style.display=="none") {
-    div.style.display = "block";
-    maindiv.innerHTML = "- Partager";
-  } else {
-    div.style.display = "none";
-    maindiv.innerHTML = "+ Partager";
-  }
-}
--- Marigolds/social/social.plugin.disabled.php
+++ Marigolds/social/social.plugin.disabled.php
@@ -4,10 +4,10 @@
 @author Cobalt74 <cobalt74@gmail.com>
 @link http://www.cobestran.com
 @licence CC by nc sa http://creativecommons.org/licenses/by-nc-sa/2.0/fr/
-@version 2.0.0
+@version 1.0.0
 @description Le plugin Social permet de partager les news avec son réseau social préféré
 */
-function Social_plugin_AddButton_old(&$link){
+function Social_plugin_AddButton(&$link){
   echo '<div class="social_sep">
         <div onclick="openURL(\'https://twitter.com/share?url='.$link->getLink().'\');" class="social_div">Tweeter</div> 
         <div onclick="openURL(\'http://www.facebook.com/share.php?u='.$link->getLink().'\');" class="social_div">Facebook</div> 
@@ -15,22 +15,9 @@ function Social_plugin_AddButton_old(&$link){
         </div>';
 }
 
-function Social_plugin_AddButton(&$event){
-  $eventId = "social_".$event->getId();
-  $link = $event->getLink();
-  
-  echo '<div class="social_group">
-          <div class="social_divbut" onclick="social_toggle_div(this,\''.$eventId.'\');">+ Partager</div>
-          <div class="social_gdiv" id="'.$eventId.'">
-            <div onclick="openURL(\'https://twitter.com/share?url='.$link.'\');" class="social_div">Tweeter</div> 
-            <div onclick="openURL(\'http://www.facebook.com/share.php?u='.$link.'\');" class="social_div">Facebook</div> 
-            <div onclick="openURL(\'https://plus.google.com/share?url='.$link.'&hl=fr\');" class="social_div">Google+</div>
-          </div>
-        </div>';
-}
-
 // Ajout de la fonction au Hook situé avant l'affichage des évenements
 Plugin::addCss("/css/style.css"); 
 Plugin::addJs("/js/main.js");
 Plugin::addHook("event_post_top_options", "Social_plugin_AddButton"); 
+Plugin::addHook("event_pre_bottom_options", "Social_plugin_AddButton");
 ?>
