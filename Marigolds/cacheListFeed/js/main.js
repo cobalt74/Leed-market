3a0b8b6e22473d81f139cbf50c7ef732a15bfc48
cobalt74 <cobalt74@gmail.com>
2013-03-30 00:45:19 +0100

    Ajout du plugin cacheListFeed qui comme son nom l'indique, cache la liste des feed pour une lecture en plein écran.

---
+++ Marigolds/cacheListFeed/js/main.js
@@ -0,0 +1,14 @@
+function cacheListFeed_toggle_div(maindiv) {
+  var divmenubar = document.getElementById('menuBar');
+  var Elem_article = document.querySelector('#main article');
+  
+  if(divmenubar.style.display=="none") {
+    divmenubar.style.display = "block";
+    Elem_article.style.width="67%";
+    document.getElementById('cacheListFeed_divbut_return').parentNode.removeChild(document.getElementById('cacheListFeed_divbut_return'));
+  } else {
+    divmenubar.style.display = "none";
+    Elem_article.style.width = "100%";
+    Elem_article.innerHTML = '<div class="cacheListFeed_divbut" id="cacheListFeed_divbut_return" title="Afficher la liste des Feeds" onclick="cacheListFeed_toggle_div(this,\'menuBar\');"><</div>'+Elem_article.innerHTML;    
+  }
+}
