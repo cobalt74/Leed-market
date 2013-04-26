950bd8e26882328fd65d1480cb1dd1751432d5bf
cobalt74 <cobalt74@gmail.com>
2013-04-26 22:44:42 +0200

    plugin social recap link en bdd

--- social/social.plugin.disabled.php
+++ social/social.plugin.disabled.php
@@ -4,18 +4,22 @@
 @author Cobalt74 <cobalt74@gmail.com>
 @link http://www.cobestran.com
 @licence CC by nc sa http://creativecommons.org/licenses/by-nc-sa/2.0/fr/
-@version 2.1.0
+@version 2.2.0
 @description Le plugin Social permet de partager les news avec son réseau social préféré
 */
 
 function Social_plugin_AddButton(&$event){
   $eventId = "social_".$event->getId();
-  $link = $event->getLink();
+  //$link = $event->getLink();
   
+  $requete = 'SELECT link FROM '.MYSQL_PREFIX.'event WHERE id = '.$event->getId();
+  $query = mysql_query($requete);
+  $link = mysql_result($query,0);
+  	
   echo '<div class="social_group">
           <div class="social_divbut" onclick="social_toggle_div(this,\''.$eventId.'\');">+ Partager</div>
           <div class="social_gdiv" id="'.$eventId.'" style="display:none">
-            <div onclick="openURL(\'https://twitter.com/share?url='.$link.'\');" class="social_div">Twitter</div> 
+            <div onclick="openURL(\'https://twitter.com/share?url='.$link.'\');" class="social_div">Tweeter</div> 
             <div onclick="openURL(\'http://www.facebook.com/share.php?u='.$link.'\');" class="social_div">Facebook</div> 
             <div onclick="openURL(\'https://plus.google.com/share?url='.$link.'&hl=fr\');" class="social_div">Google+</div>
           </div>
