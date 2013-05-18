8da8b7156e368bd87bd3e0b5da322c3ea56e635b
cobalt74 <cobalt74@gmail.com>
2013-05-18 23:21:15 +0200

    LeedBrowser avec chemin absolue + utilisation de la session en cours

--- leedBrowser/browser.php
+++ leedBrowser/browser.php
@@ -1,15 +1,12 @@
 <?php
-session_start();
-require_once('../../MysqlEntity.class.php');
-require_once('../../Event.class.php');
-require_once('../../Plugin.class.php');
-$eventManager = new Event();
+require_once("../../common.php");
+$rootPath=$configurationManager->get('root');
+
 try{
 	$event = $eventManager->getById($_GET['event']);
 } catch (Exception $e) {
     echo 'Impossible d\'ouvrir l\'évenement : ',  $e->getMessage(), "\n";
 }
-//var_dump($event);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html id="leed-browser" xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
@@ -22,8 +19,8 @@ try{
 <div id="browser-bar">
 <div id="browser-logo">L<i>eed</i> <span>browser</span></div>
 <ul id="browser-toolbar">
-	<li><button <?php  if(!$event->getUnread()){ ?>class="eventRead"<?php } ?> onclick="readUnread(this,<?php echo $_GET['event']; ?>);">Marquer comme <?php echo (!$event->getUnread()?'non lu':'lu'); ?></button></li>
-	<li><button <?php  if($event->getFavorite()){ ?>class="eventFavorite"<?php } ?> onclick="favorize(this,<?php echo $_GET['event']; ?>);"><?php echo (!$event->getFavorite()?'Favoriser':'Défavoriser'); ?></button></li>
+	<li><button <?php  if(!$event->getUnread()){ ?>class="eventRead"<?php } ?> onclick="readUnread(this,<?php echo $_GET['event'].',\''.$rootPath.'\''; ?>);">Marquer comme <?php echo (!$event->getUnread()?'non lu':'lu'); ?></button></li>
+	<li><button <?php  if($event->getFavorite()){ ?>class="eventFavorite"<?php } ?> onclick="favorize(this,<?php echo $_GET['event'].',\''.$rootPath.'\''; ?>);"><?php echo (!$event->getFavorite()?'Favoriser':'Défavoriser'); ?></button></li>
 	<?php Plugin::callHook("leed_browser_toolbar", array(&$events)); ?>
 	<li><button onclick="window.location='<?php echo $_GET['link']; ?>';">Sortir</button></li>
 </ul>
