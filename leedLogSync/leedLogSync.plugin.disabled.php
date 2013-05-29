2680a603b68a7844f2162d8df7cc47cec66e3355
cobalt74 <cobalt74@gmail.com>
2013-05-30 00:01:59 +0200

    LeedLogSync v2. liste des fichiers log disponibles. permet de résoudre partiellement l'Issue #43

--- leedLogSync/leedLogSync.plugin.disabled.php
+++ leedLogSync/leedLogSync.plugin.disabled.php
@@ -4,7 +4,7 @@
 @author Cobalt74 <cobalt74@gmail.com>
 @link http://www.cobestran.com
 @licence CC by nc sa http://creativecommons.org/licenses/by-nc-sa/2.0/fr/
-@version 1.1.0
+@version 2.0.0
 @description Le plugin permet l'affichage des logs de synchro du cron
 */
 
@@ -15,25 +15,60 @@ function leedLogSync_plugin_AddLink(){
 }
 
 // affichage des option de recherche et du formulaire
-function leedLogSync_plugin_AddForm(){
+function leedLogSync_plugin_AddForm(&$myUser){
 	echo '<section id="leedLogSync" name="leedLogSync" class="leedLogSync">
 			<h2>Logs dernière synchronisation</h2>';
+	
+	$dir    = './logs';
+	
+	$myUser = (isset($_SESSION['currentUser'])?unserialize($_SESSION['currentUser']):false);
+	if($myUser!=false) {
+		if ($myUser->getId()==1){
+			
+			$files = scandir($dir);
 
-	// Ouverture du fichier en lecture seule
-	$handle = fopen('./logs/cron.log', 'r');
-	// Si on a réussi à ouvrir le fichier
-	if ($handle)
-	{
-		// Tant que l'on est pas à la fin du fichier
-		while (!feof($handle))
-		{
-			// On lit la ligne courante
-			$buffer = fgets($handle);
-			// On l'affiche
-			echo $buffer;
+			if (isset($_POST['plugin_leedLogSync_file'])) {
+				$fileLog = $dir.'/'.$_POST['plugin_leedLogSync_file'];
+			} else {
+				$fileLog = $dir.'/'.$myUser->getLogin().'.log';
+			}
+	
+			echo '<form action="settings.php#leedLogSync" method="post">';
+			echo '  <select name="plugin_leedLogSync_file">';
+					foreach ($files as $key => $value) {
+						if (!in_array($value,array(".","..", ".htaccess", "@eadir", ".DS_Store"))) { 
+							 if (!is_dir($dir . DIRECTORY_SEPARATOR . $value)) {
+								if (isset($_POST['plugin_leedLogSync_file'])) {
+									if ($_POST['plugin_leedLogSync_file']==$value) {
+										echo '<option value="'.$value.'" selected>'.$value.'</option>'; 
+									} else {
+										echo '<option value="'.$value.'">'.$value.'</option>'; 
+									}
+								} else {
+									if ($myUser->getLogin().'.log'==$value) {
+										echo '<option value="'.$value.'" selected>'.$value.'</option>'; 
+									} else {
+										echo '<option value="'.$value.'">'.$value.'</option>'; 
+									}
+								}
+							 }
+						}
+					}
+			echo '  </select>
+					<button type="submit">Afficher</button>
+				  </form>';
+			}
+	}
+	
+	if (!isset($fileLog)) { $fileLog = $dir.'/'.$myUser->getLogin().'.log'; }
+	if (isset($_POST['plugin_leedLogSync_file'])) {
+		echo 'Affichage du fichier : '.$fileLog;
+	
+		if (file_exists($fileLog)){ 
+			print_r(file_get_contents($fileLog)); 
+		} else {
+			echo '<li>Aucun fichier de log présent. </li><li>Fichier attendu: '.$fileLog.'</li>';
 		}
-		// On ferme le fichier
-		fclose($handle);
 	}
 	echo '</section>';
 }
