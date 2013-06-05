89bb26d76b112d9dc3d500c2c6816f19738d1239
Cobalt74 <cobalt74@gmail.com>
2013-06-06 00:35:56 +0300

    Update DeleteTheCache.plugin.disabled.php - correction
    enlève les deux erreurs liées aux appel css et js qui n'existe pas

--- DeleteTheCache/DeleteTheCache.plugin.disabled.php
+++ DeleteTheCache/DeleteTheCache.plugin.disabled.php
@@ -56,17 +56,17 @@ $myUser = (isset($_SESSION['currentUser'])?unserialize($_SESSION['currentUser'])
 if($myUser==false) exit('Vous devez vous connecter pour cette action.');
     clear_folder("cache/");
 header('location: ./index.php');
-echo "<script>alert('le cache a été vidé');</script>";
+echo "<script>alert('le cache a ï¿½tï¿½ vidï¿½');</script>";
 }
 
 }
-// Ajout du css du squelette en en tête de leed
-Plugin::addCss("/css/style.css"); 
+// Ajout du css du squelette en en tï¿½te de leed
+//Plugin::addCss("/css/style.css"); 
 
 // Ajout du javascript du squelette au bas de page de leed
-Plugin::addJs("/js/main.js"); 
+//Plugin::addJs("/js/main.js"); 
  
-// Ajout de la fonction squelette_plugin_action à la page action de leed qui contient tous les traitements qui n'ont pas besoin d'affichage (ex :supprimer un flux, faire un appel ajax etc...)
+// Ajout de la fonction squelette_plugin_action ï¿½ la page action de leed qui contient tous les traitements qui n'ont pas besoin d'affichage (ex :supprimer un flux, faire un appel ajax etc...)
 Plugin::addHook('setting_post_link', 'delcache_plugin_setting_link'); 
 Plugin::addHook("action_post_case", "delcache_plugin_action"); 
 ?>
