ef85b665eda96ad00fdf00ae0b374d6ec66f4cd3
cobalt74 <cobalt74@gmail.com>
2013-06-23 23:53:52 +0200

    Plugin CacheListFeed - v2.0.0 enregistrement de la configuration caché / affiché Plugin LeedHomeLink, modification css pour une meilleur intégration.

--- cacheListFeed/cacheListFeed.plugin.disabled.php
+++ cacheListFeed/cacheListFeed.plugin.disabled.php
@@ -4,15 +4,33 @@
 @author Cobalt74 <cobalt74@gmail.com>
 @link http://www.cobestran.com
 @licence CC by nc sa http://creativecommons.org/licenses/by-nc-sa/2.0/fr/
-@version 1.1.0
+@version 2.0.0
 @description Le plugin cacheListFeed permet de cacher la liste des feed afin de lire les news en plein écran.
 */
 
 function cacheListFeed_plugin_AddButton(&$event){
-  echo '<div class="cacheListFeed_divbut" title="Cacher la liste des Feeds" onclick="cacheListFeed_toggle_div(this,\'menuBar\');">></div>';
+	echo '<div class="cacheListFeed_divbut" title="Cacher la liste des Feeds" onclick="cacheListFeed_toggle_div();">></div>';
+}
+
+function cacheListFeed_plugin_action (&$_) {
+	if ($_['action']=='cacheListFeed'){
+		$myUser = (isset($_SESSION['currentUser'])?unserialize($_SESSION['currentUser']):false);
+		if($myUser==false) exit();
+		$configurationManager = new Configuration();
+  		$configurationManager->getAll();
+		$configurationManager->put('cacheListFeed', $_['cacher']);
+	}
+	if ($_['action']=='getCacheListFeed'){
+		$myUser = (isset($_SESSION['currentUser'])?unserialize($_SESSION['currentUser']):false);
+		if($myUser==false) exit();
+		$configurationManager = new Configuration();
+  		$configurationManager->getAll();
+		echo $configurationManager->get('cacheListFeed');
+	}
 }
 
 // Ajout de la fonction au Hook situé avant l'affichage des évenements
 Plugin::addJs("/js/main.js");
-Plugin::addHook("menu_pre_folder_menu", "cacheListFeed_plugin_AddButton"); 
+Plugin::addHook("menu_pre_folder_menu", "cacheListFeed_plugin_AddButton");
+Plugin::addHook("action_post_case", "cacheListFeed_plugin_action");
 ?>
